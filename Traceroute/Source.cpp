e0e631dbd050d97dfe241af48f6850d58adf8dd3
KomarDL <48546222+KomarDL@users.noreply.github.com>
2019-03-14 11:56:21 +0300

    Add files via upload

--- Traceroute/Source.cpp
+++ Traceroute/Source.cpp
@@ -4,12 +4,19 @@
 #include <string.h>
 #include <ws2tcpip.h>
 #include <mstcpip.h>
+#include <string>
 #include <iostream>
+
 #pragma comment(lib, "ws2_32.lib")
+#pragma warning(disable : 4996)
+
 using namespace std;
+
 #define DEFAULT_PACKET_SIZE 40
 #define DEFAULT_BUFFER_SIZE 1024
-#define _WINSOCK_DEPRECATED_NO_WARNINGS_
+
+#define Err -1
+#define EndTracert 2
 
 typedef struct ICMPHeader
 {
@@ -41,6 +48,14 @@ typedef struct Packetinfo
 	unsigned long ping;
 } PacketInfo, *PPacketInfo;
 
+typedef struct _Settings
+{
+	int hops;
+	int delay;
+	int packets;
+	string Ip;
+} Settings;
+
 unsigned short calcCheckSum(unsigned short *packet) {
 	unsigned long checksum = 0;
 	int size = 40;
@@ -56,7 +71,7 @@ unsigned short calcCheckSum(unsigned short *packet) {
 	return (unsigned short)(~checksum);
 }
 
-void initPingPacket(PICMP sendHdr, unsigned char seq)
+void initPing(PICMP sendHdr, unsigned char seq)
 {
 	sendHdr->Type = 8;
 	sendHdr->Code = 0;
@@ -66,7 +81,7 @@ void initPingPacket(PICMP sendHdr, unsigned char seq)
 	sendHdr->Checksum = calcCheckSum((unsigned short *)sendHdr);
 }
 
-int sendPingReq(SOCKET sock, PICMP sendBuf, const struct sockaddr_in *dst)
+int sendPing(SOCKET sock, PICMP sendBuf, const struct sockaddr_in *dst)
 {
   //сокет, указатель на буфер с данными, размер буфера с данными, флаги определяющие способ выполн вызова, адрес сокета на который шлём, размер структуры
 	int Res = sendto(sock, (char *)sendBuf, DEFAULT_PACKET_SIZE, 0, (struct sockaddr *)dst, sizeof(struct sockaddr_in));
@@ -76,7 +91,7 @@ int sendPingReq(SOCKET sock, PICMP sendBuf, const struct sockaddr_in *dst)
 	return 0;
 }
 
-int decodeReply(PIP ipHdr, struct sockaddr_in *src, unsigned short seq, unsigned long SendTime, PPacketInfo decodeResult)
+int answDecode(PIP ipHdr, struct sockaddr_in *src, unsigned short seq, unsigned long SendTime, PPacketInfo decodeResult)
 {
 //в ответ нам приходит ip пакет в котором лежит ICMP пакет
 //если тип пакета 11 значит время жизни истекло и в пакете лежит 
@@ -115,14 +130,14 @@ int decodeReply(PIP ipHdr, struct sockaddr_in *src, unsigned short seq, unsigned
 		{
 			decodeResult->ping = arrivalTime - SendTime;
 			decodeResult->src = src;
-			return 2;
+			return EndTracert;
 		}
 	}
 
-	return -1;
+	return Err;
 }
 
-int recvPing(SOCKET sock, PIP recvBuf, struct sockaddr_in *src)
+int recvPing(SOCKET sock, PIP recvBuf, struct sockaddr_in *src, int delay)
 {
 	int srcLen = sizeof(struct sockaddr_in);
   //структура для тестирования сокета на читаемость с помощью select
@@ -130,7 +145,7 @@ int recvPing(SOCKET sock, PIP recvBuf, struct sockaddr_in *src)
 	singleSocket.fd_count = 1;
 	singleSocket.fd_array[0] = sock;
   //задаём интервал ожидания 2 секунды 0 милисекунд
-	struct timeval timeToWait = { 2, 0 };
+	struct timeval timeToWait = { delay, 0 };
 
   //ждём ответ 2 секунды
   //select вернёт количество сокетов которые можно читать и если вернуло 0 то время ожидания истекло
@@ -147,7 +162,7 @@ int recvPing(SOCKET sock, PIP recvBuf, struct sockaddr_in *src)
 
 void print(PPacketInfo info, BOOL printIP)
 {
-	printf("%6d", info->ping);
+	printf_s("%6d", info->ping);
 
 	if (printIP)
 	{
@@ -155,26 +170,78 @@ void print(PPacketInfo info, BOOL printIP)
 		char *srcAddr = inet_ntoa(info->src->sin_addr);
 		if (srcAddr != NULL) 
 		{
-			printf("\t%s", srcAddr);
+			printf_s("\t%s", srcAddr);
 		}
 		else
 		{
-			printf("unknown IP");
+			printf_s("unknown IP");
 		}
 		char hbuf[NI_MAXHOST];
 		if (!getnameinfo((struct sockaddr *)(info->src), sizeof(struct sockaddr_in), hbuf, sizeof(hbuf), NULL, 0, NI_NAMEREQD))
-			printf(" %s", hbuf);
+			printf_s(" %s", hbuf);
+	}
+}
+
+void Hint(Settings *setting)
+{
+	cout << "Параметры для настройки\n";
+
+	cout << "Значения по умолчанию:\n\tIP = " << setting->Ip << "\n\tКоличество посланных пакетов = " << setting->packets << '\n';
+	cout << "\tМаксимальное время жизни пакета = " << setting->hops << "\n\tВремя ожидания ответа = " << setting->delay << '\n';
+}
+
+void UserInput(Settings *setting)
+{
+	string answ;
+	cout << "Изменить стандартные настройки?\n";
+	do
+	{
+		cout << "Ответ(y/Y, n/N): ";
+		getline(cin, answ);
+	} while (answ[0] != 'y' && answ[0] != 'n' && answ[0] == 'Y' && answ[0] == 'N');
+
+	if (answ[0] == 'y' || answ[0] == 'Y')
+	{
+		cout << "Нажмите <Enter> если хотите оставить стандартное значени\n";
+		cout << "IP = ";
+		answ = "";
+		getline(cin, answ);
+		if (answ.length() != 0)
+			setting->Ip = answ;
+
+		cout << "Количество пакетов = ";
+		answ = "";
+		getline(cin, answ);
+		if (answ.length() != 0)
+			setting->packets = stoi(answ);
+
+		cout << "Максимальное время жизни пакета = ";
+		answ = "";
+		getline(cin, answ);
+		if (answ.length() != 0)
+			setting->hops = stoi(answ);
+
+		cout << "Время ожидания ответа = ";
+		answ = "";
+		getline(cin, answ);
+		if (answ.length() != 0)
+			setting->delay = stoi(answ);
 	}
 }
 
 int main(int argc, char *argv[])
 {
 	setlocale(LC_ALL, "Russian");
- 
+	
+	Settings setting = { 30, 2, 3, "8.8.8.8"};
+	
+	Hint(&setting);
+	UserInput(&setting);
 //инициализация библиотеки
 	WSADATA wsaData;
-	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
-		std::cout << "error" << std::endl;
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+	{
+		cout << "error" << endl;
 		exit(1);
 	}
 	
@@ -185,65 +252,69 @@ int main(int argc, char *argv[])
 	SOCKET sock = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
 	if (sock == INVALID_SOCKET)
 	{
-		wprintf(L"socket failed with error %d\n", WSAGetLastError());
+		printf_s("Socket failed with error %d\n", WSAGetLastError());
 		return 1;
 	}
 
 	PacketInfo info;
 	int ttl = 0;
-	int number = 1;
+	int numb = 1;
 	unsigned char seq = 0;
 	unsigned long SendTime;
 
-	int hops = 30;
 	BOOL traceEnd = FALSE, error = FALSE, printIP;
 
+//конвертируем из std::string в char*
+	char *cstr = new char[setting.Ip.length() + 1];
+	strcpy(cstr, setting.Ip.c_str());
 //из строки в IPv4
-	dst.sin_addr.s_addr = inet_addr("8.8.8.8"/*argv[1]*/);
+	dst.sin_addr.s_addr = inet_addr(cstr);
 	dst.sin_family = AF_INET;
+	free(cstr);
 
-	cout << "Трассировка маршрута к " << "8.8.8.8"/*argv[1]*/ <<  endl;
-	cout << "с максимальным числом прыжков 30:" << endl;
+	system("cls");
+	cout << "Трассировка маршрута к " << setting.Ip <<  endl;
+	cout << "с максимальным числом прыжков " << setting.hops << ": " << endl;
 	do
 	{
 		setsockopt(sock, IPPROTO_IP, IP_TTL, (char *)&(++ttl), sizeof(int));
 
 		printIP = FALSE;
-		printf("%3d.", number++);
+		printf_s("%3d.", numb++);
 		
-		for (int i = 1; i <= 3; i++) 
+		for (int i = 1; i <= setting.packets; i++) 
 		{
-		//если уже получили 3 пакета то можно вывести netbios имя(если есть)
-			if (i == 3)
+		//если уже получили packets пакетов то можно вывести netbios имя(если есть)
+			if (i == setting.packets)
 				printIP = TRUE;
 		  //seq должен быть всегда разный
-			initPingPacket(sendBuf, ++seq);
+			initPing(sendBuf, ++seq);
 			SendTime = GetTickCount();
-			sendPingReq(sock, sendBuf, &dst);
+			sendPing(sock, sendBuf, &dst);
 			int recvRes = 2;
-			int decodeRes = -1; // ошибка
+			int decodeRes = Err;
 			
 		//получаем количество принятых байт
-			recvRes = recvPing(sock, recvBuf, &src);
+			recvRes = recvPing(sock, recvBuf, &src, setting.delay);
 		//если получено 0 байт значит время ожидания истекло
 			if (recvRes == 0) 
 			{
-				printf("\t*");
+				printf_s("\t*");
 			}
 			else
 			{
-				decodeRes = decodeReply(recvBuf, &src, seq, SendTime, &info);
+				decodeRes = answDecode(recvBuf, &src, seq, SendTime, &info);
 			}
 			
 			if (recvRes > 1) 
 			{
-				if (decodeRes == -1) 
+				if (decodeRes == Err) 
 				{
-					printf("\t*");
+					printf_s("\t*");
 				}
 				else 
 				{
-					if (decodeRes == 2) 
+					if (decodeRes == EndTracert) 
 					{
 						traceEnd = TRUE;
 					}
@@ -251,7 +322,7 @@ int main(int argc, char *argv[])
 				}
 			}
 		}
-		printf("\n");
-	} while (!traceEnd && (ttl != hops));
+		printf_s("\n");
+	} while (!traceEnd && (ttl != setting.hops));
 		system("pause");
 }
